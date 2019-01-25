8f8d665d9aee01f6f3c12a23498f8944175d4220
Ashwin Vidiyala <ashwinvidiyala@gmail.com>
2019-01-25 17:59:10 -0600

    Build a working SMS Classifier with a complete dataset

---
+++ README.md
@@ -0,0 +1,8 @@
+# SMS Classifier
+
+A simple implementation of a Bayesian Classification using the 
+[classifier-reborn](https://github.com/jekyll/classifier-reborn) gem.
+
+The [SMS Spam Collection
+v.1](http://www.dt.fee.unicamp.br/~tiago//smsspamcollection/) dataset was used
+(thanks guys).
---
+++ sms_classifier.rb
@@ -0,0 +1,20 @@
+require 'classifier-reborn'
+
+classifier = ClassifierReborn::Bayes.new('spam', 'not spam')
+
+f = File.open('dataset/sms_spam_collection.txt', 'r')
+line_number = 0
+f.each_line do |line|
+  line_number += 1
+  sentence = line.split(' ')
+  category = sentence.shift == 'spam' ? 'spam' : 'not spam'
+  # puts "#{category}: #{sentence.join(' ')}"
+  classifier.train(category, sentence.join(' '))
+end
+
+puts "The algorithm was trained with #{line_number} pieces of data"
+puts
+puts 'What is thy question for the almighty God of SMS Classification?'
+question = gets.chomp
+puts
+puts "The above message is #{classifier.classify question}"
